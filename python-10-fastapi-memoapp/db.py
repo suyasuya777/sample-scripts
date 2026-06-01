0b4ee8c8f95250fbc3c3c03bacb6f6d6e7e44e99
Yasuo Nakata <chasoo1@yahoo.co.jp>
2026-06-01 21:38:10 +0900

    19nd commit

--- python-10-fastapi-memoapp/db.py
+++ python-10-fastapi-memoapp/db.py
@@ -1,6 +1,7 @@
 import os
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
 
 # ==================================================
 # DBアクセス
@@ -11,17 +12,17 @@ Base = declarative_base()
 # DBファイル作成
 base_dir = os.path.dirname(__file__)
 # データベースのURL
-DATABASE_URL = 'sqlite+aiosqlite:///' + os.path.join(base_dir, 'memodb.sqlite')
+DATABASE_URL = "sqlite+aiosqlite:///" + os.path.join(base_dir, "memodb.sqlite")
 
 # 非同期エンジンの作成
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 # 非同期セッションの設定
-async_session = sessionmaker(
+async_session = async_sessionmaker(
     engine,
     expire_on_commit=False,
-    class_=AsyncSession
 )
+
 
 # DBとのセッションを非同期的に扱うことができる関数
 async def get_dbsession():
